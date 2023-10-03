02c6287139718b2582efd9343bbd4d2008bab98b
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-10-03 11:34:40 +0100

    m

--- C/C++/Array_init.c
+++ C/C++/Array_init.c
@@ -8,13 +8,33 @@
 
 int main(int argc, char const *argv[])
 {
-    
-    int arr[4]={2}; /* when you set only some elements the rest is set to 0s */
-    int length=sizeof(arr)/sizeof(arr[0]);
+    int arr0[]={1,2,3,4,5,5};
+    int arr1[3];     /* for this one the elements will have garbage values , 
+                        if the result is 0s its just a coincidence   */
+    int arr2[4]={2}; /* when you set only some elements the rest is set to 0s */
 
-    for (int i = 0; i < length; i++) {
-        printf("%d ", arr[i]);
+
+    int length0=sizeof(arr0)/sizeof(arr0[0]);
+    int length1=sizeof(arr1)/sizeof(arr1[0]);
+    int length2=sizeof(arr2)/sizeof(arr2[0]);
+
+
+    printf("\n%s\n","printing the values of array arr0 ::");
+    for (int i = 0; i < length0; i++) {
+        printf("%d ", arr0[i]);
     }
+
+    printf("\n%s\n","printing the values of array arr1 ::");
+    for (int i = 0; i < length1; i++) {
+        printf("%d ", arr1[i]);
+    }
+
+    printf("\n%s\n","printing the values of array arr2 ::");
+    for (int i = 0; i < length2; i++) {
+        printf("%d ", arr2[i]);
+    }
+
+    
 
     return 0;
 }
